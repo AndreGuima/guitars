cc3a874ad4a071d220d8e8d563dff954185a9caf
AndreGuima <andrepaivaguimaraes@gmail.com>
2019-04-18 13:52:32 -0300

    add sonar bug fixes

---
+++ src/main/java/com/adpguima/dto/GuitarDTO.java
@@ -0,0 +1,15 @@
+package com.adpguima.dto;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GuitarDTO {
+	private String brand;
+	private String model;
+	private Date manufactureDate;
+
+}
--- src/main/java/com/adpguima/service/GuitarService.java
+++ src/main/java/com/adpguima/service/GuitarService.java
@@ -2,6 +2,8 @@ package com.adpguima.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import com.adpguima.repository.GuitarRepository;
 @Service
 public class GuitarService {
 
+	private static final Logger log = LoggerFactory.getLogger(GuitarService.class);
+
 	@Autowired
 	GuitarRepository guitarRepository;
 
@@ -20,6 +24,8 @@ public class GuitarService {
 	}
 
 	public Guitar add(Guitar guitarAdd) {
+
+		log.info("New Guitar is created with id: {0} ", guitarAdd.getId());
 		return guitarRepository.save(guitarAdd);
 	}
 
